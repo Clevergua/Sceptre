0fe2c588dae0ba379daa391132c07d24ce7494bd
clevergua <clevergua@outlook.com>
2022-12-06 19:28:09 +0800

    Update

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class GameState : MonoBehaviour
 {
+    [SerializeField]
+    private AssetBundle assetBundle;
+    [SerializeField]
+    private Object[] objects;
+    [SerializeField]
+    private AssetBundleManifest assetBundleManifest;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(DownloadAssetBundle());
+    }
+    IEnumerator DownloadAssetBundle()
+    {
+        var path = Path.Combine(Application.streamingAssetsPath, "lua_scripts");
+        var unityWebRequest = UnityWebRequestAssetBundle.GetAssetBundle(path);
+        yield return unityWebRequest.SendWebRequest();
+        assetBundle = DownloadHandlerAssetBundle.GetContent(unityWebRequest);
+        objects = assetBundle.LoadAllAssets();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
