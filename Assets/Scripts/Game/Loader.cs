7145814108cd27c838babb99b4d9b6476448f4f9
clevergua <clevergua@outlook.com>
2022-12-08 16:16:53 +0800

    Update

---
+++ Assets/Scripts/Game/Loader.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class Loader : MonoBehaviour
+{
+    // [SerializeField]
+    // AssetReference ar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var handle = Addressables.LoadAssetAsync<Material>("Assets/UIAssets/material.mat");
+        handle.Completed += (handle) =>
+        {
+            gameObject.GetComponent<MeshRenderer>().sharedMaterial = handle.Result;
+        };
+        var handle2 = Addressables.LoadAssetAsync<GameObject>("Assets/Characters/Cube.prefab");
+        handle2.Completed += (handle2) =>
+        {
+            var r = handle2.Result;
+            Instantiate(r);
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
